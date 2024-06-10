f70a524689f3876569cafc6f9ad60c30a0a85bfb
cenkbardakci <cenkbardakci@gmail.com>
2024-06-10 20:19:45 +0200

    lock fix

--- provider.tf
+++ provider.tf
@@ -1,12 +1,19 @@
 provider "aws" {
     alias  = "eks"
-    region = "us-west-2"
+    region = "us-east-1"
 }
+
 
 provider "kubernetes" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
+
+
+provider "aws" {
+    alias  = "s3"
+    region = "us-east-1"
 }
 
 terraform {
